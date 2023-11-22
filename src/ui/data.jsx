0780c39cdbe400965a8ad46997260f116fc4023d
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2023-11-22 21:02:01 +0530

    Update data.jsx

--- src/ui/data.jsx
+++ src/ui/data.jsx
@@ -1,19 +1,15 @@
-const randomNum = parseInt(Math.random()*1)+35;
 const data = [{
     title:'Specific Image',
     desc:'Get a specific image by adding /number/ to the start of the url.',
     link:'https://museum-api-mocha.vercel.app/{specific number}',
-    img_link:'ie. https://museum-api-mocha.vercel.app/1',
-    image:'https://github.com/Niteshkumar13/museum_api/blob/main/images/42.webp?raw=true'
+    img_link:'ie. https://museum-api-mocha.vercel.app/1'
 
 },
 {
     title:'Static Random Image',
     desc:'Get the same random image every time based on a seed, by adding /{random} to the start of the url.',
     link:'https://museum-api-mocha.vercel.app/random',
-    img_link:'ie. https://museum-api-mocha.vercel.app/random',
-    image:`https://github.com/Niteshkumar13/museum_api/blob/main/images/${randomNum}.jpg?raw=true`
- 
+    img_link:'ie. https://museum-api-mocha.vercel.app/random'
 }
 ]
 export default data
